101ca1e348bdef9d4ea94e6c482b8a5b490c13c7
AllyTally <alexiatilde@gmail.com>
2021-01-30 02:29:48 -0400

    start working on mob taker (working title)

---
+++ src/main/java/moe/ally/nameless/MobTakerItem.java
@@ -0,0 +1,93 @@
+package moe.ally.nameless;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.*;
+
+public class MobTakerItem extends Item {
+
+    public MobTakerItem(Settings settings) {
+        super(settings);
+    }
+
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        CompoundTag tag = stack.getOrCreateTag();
+        if (entity == null || stack == null || !entity.isAlive() || tag.contains("entity") || entity instanceof PlayerEntity) {
+            return ActionResult.FAIL;
+        }
+        if (!user.world.isClient) {
+            entity.stopRiding();
+            entity.removeAllPassengers();
+            CompoundTag entityData = new CompoundTag();
+            if (!entity.saveToTag(entityData)) return ActionResult.FAIL;
+
+            tag.put("entity", entityData);
+            entity.remove();
+
+            user.setStackInHand(hand, stack);
+        }
+        user.playSound(SoundEvents.ITEM_BOTTLE_FILL_DRAGONBREATH, 1.0F, 1.0F);
+        return ActionResult.success(user.world.isClient);
+    }
+
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        BlockPos pos = context.getBlockPos();
+        World world = context.getWorld();
+        BlockEntity tileEntity = world.getBlockEntity(pos);
+        ItemStack stack = context.getStack();
+
+        if (world.isClient) return ActionResult.PASS;
+
+        BlockPos offset_pos = pos.offset(context.getSide());
+        CompoundTag tag = stack.getOrCreateTag();
+        tag.remove("entity");
+        CompoundTag compoundTag = (CompoundTag) tag.get("entity");
+        compoundTag.remove("Passengers");
+        compoundTag.remove("Leash");
+        compoundTag.remove("UUID");
+        //compoundTag.put("Pos", offset_pos.asLong());
+        //compoundTag.put("Motion", (0.0, 0.0, 0.0));
+
+
+        //Entity entity = EntityType.loadEntityWithPassengers(compoundTag, world, (entityx) -> {
+        //    return entityx;
+        //};
+
+        /*
+*/
+        //world.spawnEntity(entity);
+        EntityType.getEntityFromTag(compoundTag, world);
+
+        world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_FILL_DRAGONBREATH, SoundCategory.BLOCKS, 1.0F, 1.0F);
+        return ActionResult.success(true);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        if (itemStack.getOrCreateTag().contains("entity")) {
+            //tooltip.add("aaaaaaaaaaaaaaaaaaaaaaaaaa");
+        }
+    }
+
+    @Override
+    public boolean hasGlint(ItemStack stack) {
+        return stack.getOrCreateTag().contains("entity");
+    }
+}
--- src/main/java/moe/ally/nameless/Nameless.java
+++ src/main/java/moe/ally/nameless/Nameless.java
@@ -18,6 +18,7 @@ import net.minecraft.util.registry.Registry;
 
 public class Nameless implements ModInitializer {
 	public static final TimeInABottleItem TIME_IN_A_BOTTLE = new TimeInABottleItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1));
+	public static final MobTakerItem MOB_TAKER = new MobTakerItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1));
 	public static final SlimeslingItem SLIMESLING = new SlimeslingItem(new FabricItemSettings().group(ItemGroup.TOOLS).maxCount(1));
 	public static final SpikeBlock SPIKE_BLOCK = new SpikeBlock(Block.Settings.of(Material.STONE).strength(4.0f));
 	public static final ArmorMaterial slimeBootsMaterial = new SlimeBootsMaterial();
@@ -34,6 +35,7 @@ public class Nameless implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("nameless", "time_in_a_bottle"), TIME_IN_A_BOTTLE);
+		Registry.register(Registry.ITEM, new Identifier("nameless", "mob_taker"), MOB_TAKER);
 		Registry.register(Registry.ITEM, new Identifier("nameless", "slimesling"), SLIMESLING);
 		Registry.register(Registry.ITEM, new Identifier("nameless", "slime_boots"), SLIME_BOOTS);
 		Registry.register(Registry.BLOCK, new Identifier("nameless", "spike_block"), SPIKE_BLOCK);
